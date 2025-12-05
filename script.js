9fcd2ff8aa1a87a06e48874ec7d550eee154b78c
Yelishetty Vedhanth <yelishettyvedhanth@Yelishettys-MacBook-Air.local>
2025-12-05 13:34:37 +0530

    upadated code

--- script.js
+++ script.js
@@ -3,43 +3,43 @@ function showInventor() {
 
     const data = {
         gosling: {
-            img: "/Users/yelishettyvedhanth/Downloads/gossling.jpeg",
+            img: "/Users/yelishettyvedhanth/Library/Mobile Documents/com~apple~CloudDocs/cdc/jfs/JFS_VISEM/inventors/gossling.jpeg",
             info: "James Gosling is the creator of Java, developed at Sun Microsystems in 1995."
         },
         rossum: {
-            img: "/Users/yelishettyvedhanth/Downloads/rossum.jpeg",
+            img: "/Users/yelishettyvedhanth/Library/Mobile Documents/com~apple~CloudDocs/cdc/jfs/JFS_VISEM/inventors/rossum.jpeg",
             info: "Guido van Rossum created Python in 1991 and is known as the 'Benevolent Dictator For Life' of Python."
         },
         ritchie: {
-            img: "/Users/yelishettyvedhanth/Downloads/ritchii.jpeg",
+            img: "/Users/yelishettyvedhanth/Library/Mobile Documents/com~apple~CloudDocs/cdc/jfs/JFS_VISEM/inventors/ritchii.jpeg",
             info: "Dennis Ritchie created the C programming language at Bell Labs in 1972."
         },
         stroustrup: {
-            img: "/Users/yelishettyvedhanth/Downloads/stroustrup.jpeg",
+            img: "/Users/yelishettyvedhanth/Library/Mobile Documents/com~apple~CloudDocs/cdc/jfs/JFS_VISEM/inventors/stroustrup.jpeg",
             info: "Bjarne Stroustrup developed C++ as an extension of C beginning in 1979."
         },
         eich: {
-            img: "/Users/yelishettyvedhanth/Downloads/eich.jpeg",
+            img: "/Users/yelishettyvedhanth/Library/Mobile Documents/com~apple~CloudDocs/cdc/jfs/JFS_VISEM/inventors/eich.jpeg",
             info: "Brendan Eich created JavaScript in 1995 while working at Netscape."
         },
         lerdorf: {
-            img: "/Users/yelishettyvedhanth/Downloads/lerdorf.jpeg",
+            img: "/Users/yelishettyvedhanth/Library/Mobile Documents/com~apple~CloudDocs/cdc/jfs/JFS_VISEM/inventors/lerdorf.jpeg",
             info: "Rasmus Lerdorf created PHP in 1994 originally as CGI scripts."
         },
         matsumoto: {
-            img: "/Users/yelishettyvedhanth/Downloads/Yukihiro Matsumoto.jpeg",
+            img: "/Users/yelishettyvedhanth/Library/Mobile Documents/com~apple~CloudDocs/cdc/jfs/JFS_VISEM/inventors/Yukihiro Matsumoto.jpeg",
             info: "Yukihiro 'Matz' Matsumoto created Ruby in 1995 focusing on simplicity and productivity."
         },
         hejlsberg: {
-            img: "/Users/yelishettyvedhanth/Downloads/Anders Hejlsberg .jpeg",
+            img: "/Users/yelishettyvedhanth/Library/Mobile Documents/com~apple~CloudDocs/cdc/jfs/JFS_VISEM/inventors/Anders Hejlsberg .jpeg",
             info: "Anders Hejlsberg developed C# for Microsoft in 2000."
         },
         hopper: {
-            img: "/Users/yelishettyvedhanth/Downloads/race Hopper.jpeg",
+            img: "/Users/yelishettyvedhanth/Library/Mobile Documents/com~apple~CloudDocs/cdc/jfs/JFS_VISEM/inventors/race Hopper.jpeg",
             info: "Grace Hopper created COBOL and pioneered compiler technology."
         },
         mccarthy: {
-            img: "/Users/yelishettyvedhanth/Downloads/John McCarthy.jpeg",
+            img: "/Users/yelishettyvedhanth/Library/Mobile Documents/com~apple~CloudDocs/cdc/jfs/JFS_VISEM/inventors/John McCarthy.jpeg",
             info: "John McCarthy created Lisp in 1958 and is considered one of the founders of AI."
         }
     };
